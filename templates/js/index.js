9f8e301291cbe83ccdfb4578ae13c5f6a3bbe934
Leila-Jadoon <leilajadoon@gmail.com>
2022-05-09 05:26:41 -0700

    navbar shadow on scroll

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,14 +1,24 @@
 // This makes nav bar disappear when user scrolls up
-var lastScrollTop;
-navbar = document.getElementById('navbar');
+// var lastScrollTop;
+// navbar = document.getElementById('navbar');
 
+// window.addEventListener('scroll',function(){
+//     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     if (scrollTop > lastScrollTop){
+//         navbar.style.top='-80px';
+//     }
+//     else {
+//         navbar.style.top='0';
+//     }
+//     lastScrollTop = scrollTop;
+// });
+
+navbar = document.getElementById('navbar');
 window.addEventListener('scroll',function(){
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop){
-        navbar.style.top='-80px';
+    if (scrollTop > 10) {
+        navbar.classList.add('floatingNav');
+    } else {
+        navbar.classList.remove('floatingNav');
     }
-    else {
-        navbar.style.top='0';
-    }
-    lastScrollTop = scrollTop;
 });
